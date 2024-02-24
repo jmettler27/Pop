65cbdc1c2b7458fc70eaa9a85032f4f397ddfe31
julien <julien.mettler@epfl.ch>
2024-02-24 22:40:59 +0100

    Make Firebase project ID public

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -25,7 +25,7 @@ export const authOptions = {
     ],
     adapter: FirestoreAdapter({
         credential: cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
+            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
             privateKey: process.env.FIREBASE_PRIVATE_KEY
                 ? JSON.parse(process.env.FIREBASE_PRIVATE_KEY)
